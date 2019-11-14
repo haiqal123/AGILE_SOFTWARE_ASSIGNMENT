74c6d79593ec1f2fbaccb7a6b34aa7b76621359d
haiqal123 <39889193+haiqal123@users.noreply.github.com>
2019-11-14 20:24:17 +0800

    Add files via upload
    Second development stage

---
+++ Agile Assignment (Second)/Agile Assignment/ABC Coffee.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BindData()
+    End Sub
+
+    Private Sub BindData2()
+        Dim db As New StorageItemDataContext()
+        Dim rs = From o In db.Items
+                 Where o.ItemID.Contains(txtID.Text)
+
+        dgv.DataSource = rs
+    End Sub
+
+    Private Sub BindData()
+        Dim db As New StorageItemDataContext()
+        Dim rs = From o In db.Items
+                 Where o.ItemName.Contains(txtName.Text)
+
+        dgv.DataSource = rs
+    End Sub
+
+    Private Sub txtName_TextChanged(sender As Object, e As EventArgs) Handles txtName.TextChanged
+        BindData()
+    End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        frmAdd.ShowDialog()
+        BindData()
+    End Sub
+
+    Private Sub txtID_TextChanged(sender As Object, e As EventArgs) Handles txtID.TextChanged
+        BindData2()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        frmUpdate.Show()
+    End Sub
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        BindData()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        frmDelete.Show()
+    End Sub
+End Class
---
+++ Agile Assignment (Second)/Agile Assignment/frmAdd.vb
@@ -0,0 +1,35 @@
+﻿Public Class frmAdd
+    Private Sub BtnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmAdd_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        ResetForm()
+    End Sub
+    Private Sub ResetForm()
+        txtId.Clear()
+        txtName.Clear()
+        mskPrice.Clear()
+        txtDescription.Clear()
+        txtId.Focus()
+    End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim s As New Item()
+        s.ItemID = txtId.Text
+        s.ItemName = txtName.Text
+        s.ItemPrice = mskPrice.Text
+        s.ItemDescription = txtDescription.Text
+
+        Dim db As New StorageItemDataContext()
+        db.Items.InsertOnSubmit(s)
+        db.SubmitChanges()
+        MessageBox.Show("Item " & s.ItemName & " had been added successfully.", "Register", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        ResetForm()
+    End Sub
+
+    Private Sub frmAdd_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class

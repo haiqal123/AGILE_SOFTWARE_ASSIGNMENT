0d31abe47e37957b28dde08968c04f513994a743
weilun97 <51224594+weilun97@users.noreply.github.com>
2019-11-28 22:51:41 +0800

    Add files via upload

---
+++ Agile Assignment (WeiLun)/Agile Assignment/ABC Coffee.Designer.vb
@@ -0,0 +1,233 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.lblCoffee = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dgv
+        '
+        Me.dgv.AllowUserToAddRows = False
+        Me.dgv.AllowUserToDeleteRows = False
+        Me.dgv.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(9, 170)
+        Me.dgv.Margin = New System.Windows.Forms.Padding(2)
+        Me.dgv.Name = "dgv"
+        Me.dgv.ReadOnly = True
+        Me.dgv.RowHeadersWidth = 51
+        Me.dgv.RowTemplate.Height = 24
+        Me.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgv.Size = New System.Drawing.Size(592, 284)
+        Me.dgv.TabIndex = 0
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(256, 32)
+        Me.lblName.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(81, 13)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "Product Name: "
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(340, 30)
+        Me.txtName.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(127, 20)
+        Me.txtName.TabIndex = 2
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(9, 495)
+        Me.btnAdd.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(106, 38)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "Add Product"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Location = New System.Drawing.Point(14, 32)
+        Me.lblID.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(61, 13)
+        Me.lblID.TabIndex = 4
+        Me.lblID.Text = "Product ID:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.txtID)
+        Me.GroupBox1.Controls.Add(Me.lblID)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.lblName)
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 75)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Size = New System.Drawing.Size(582, 91)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Search Product By"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(77, 32)
+        Me.txtID.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(121, 20)
+        Me.txtID.TabIndex = 5
+        '
+        'lblCoffee
+        '
+        Me.lblCoffee.AutoSize = True
+        Me.lblCoffee.Font = New System.Drawing.Font("Brush Script MT", 19.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCoffee.ForeColor = System.Drawing.Color.Aqua
+        Me.lblCoffee.Location = New System.Drawing.Point(136, 25)
+        Me.lblCoffee.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblCoffee.Name = "lblCoffee"
+        Me.lblCoffee.Size = New System.Drawing.Size(374, 33)
+        Me.lblCoffee.TabIndex = 6
+        Me.lblCoffee.Text = "ABC Coffee Shop Inventory System"
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(488, 495)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(104, 38)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(169, 495)
+        Me.btnUpdate.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(109, 38)
+        Me.btnUpdate.TabIndex = 8
+        Me.btnUpdate.Text = "Update Details"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(326, 495)
+        Me.btnDelete.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(110, 38)
+        Me.btnDelete.TabIndex = 9
+        Me.btnDelete.Text = "Delete Product"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'lblCount
+        '
+        Me.lblCount.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCount.Location = New System.Drawing.Point(11, 456)
+        Me.lblCount.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(137, 27)
+        Me.lblCount.TabIndex = 10
+        Me.lblCount.Text = "x item(s)"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(340, 63)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(127, 20)
+        Me.TextBox1.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(234, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 13)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Product Description:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(610, 543)
+        Me.Controls.Add(Me.lblCount)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblCoffee)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.dgv)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "ABC Coffee Shop"
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgv As DataGridView
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents lblID As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents lblCoffee As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents lblCount As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
